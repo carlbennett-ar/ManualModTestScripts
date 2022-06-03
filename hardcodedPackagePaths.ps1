7e78c0af5268feefeea0a706cbeb0cadd3747161
carlbennett-ar <carl.bennett@applicationreadiness.com>
2022-06-03 11:06:17 +0930

    dir

--- hardcodedPackagePaths.ps1
+++ hardcodedPackagePaths.ps1
@@ -16,6 +16,6 @@ $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\7-Zip\My 7zip Shortcut.lnk')
 $Shortcut.TargetPath = $mainExePath
 $Shortcut.Arguments = """$installdir"""
-$Shortcut.WorkingDir = $installdir
+$Shortcut.WorkingDirectory = $installdir
 $Shortcut.Description = "7-zip shortcut including lots of paths"
 $Shortcut.Save()
