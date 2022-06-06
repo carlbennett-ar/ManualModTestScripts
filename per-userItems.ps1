667d8fb37f68cb9c8764c3bfdcd871f40d7e89ee
carlbennett-ar <carl.bennett@applicationreadiness.com>
2022-06-06 12:52:44 +0930

    bugfix

--- per-userItems.ps1
+++ per-userItems.ps1
@@ -32,16 +32,13 @@ xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.
 Set-Content -Path "$env:USERPROFILE\Documents\MyDocsDir.rtf" -value '{\rtf1}'
 
 md "$env:LOCALAPPDATA\LocalAppDataSubDir"
-Set-Content -Path "$env:USERPROFILE\LocalAppDataSubDir\LocalAppDataSubDir.xml" -value '<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+Set-Content -Path "$env:LOCALAPPDATA\LocalAppDataSubDir\LocalAppDataSubDir.xml" -value '<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.org/xsd/settings-1.0.0.xsd">`n  <localRepository>${user.home}/.m2/repository</localRepository>`n  <interactiveMode>true</interactiveMode>`n  <offline>false</offline>`n</settings>'
 
 md "$env:APPDATA\AppDataSubDir"
-Set-Content -Path "$env:USERPROFILE\AppDataSubDir\AppDataSubDir.xml" -value '<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+Set-Content -Path "$env:APPDATA\AppDataSubDir\AppDataSubDir.xml" -value '<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.org/xsd/settings-1.0.0.xsd">`n  <Repository>${user.home}/.m2/repository</Repository>`n  <interactiveMode>true</interactiveMode>`n  <offline>false</offline>`n</settings>'
 
-
-#inifile
-Set-Content -path ($installdir + "settings.ini") -Value "[section]`nmainExePath=$mainexepath`nINSTALLDIR=$installdir`nSys32=C:\windows\system32\`nINSTALLDIRNoTrailingSlash=$INSTALLDIRNoTrailingSlash" 
 
 #environment vars
 [Environment]::SetEnvironmentVariable('path', "$($env:Path);$installdir", 'User')
