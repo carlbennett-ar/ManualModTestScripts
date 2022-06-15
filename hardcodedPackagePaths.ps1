3864f7bc354bfe410d5d6521b4f9fafc4272e492
carlbennett-ar <carl.bennett@applicationreadiness.com>
2022-06-15 15:05:17 +0930

    new modular version

--- hardcodedPackagePaths.ps1
+++ hardcodedPackagePaths.ps1
@@ -1,32 +1,4 @@
-function writereg($key, $name, $value){
-    If  ( -Not ( Test-Path "Registry::$Key")){New-Item -Path "Registry::$Key" -ItemType RegistryKey -Force}
-    Set-ItemProperty -path "Registry::$Key" -Name $name  -Value $value 
-
-}
-$mainExePath = "C:\Program Files\7-Zip\7zFM.exe"
-$installdir = "C:\Program Files\7-Zip\"
-$INSTALLDIRNoTrailingSlash = "C:\Program Files\7-Zip"
-
-#set registry keys
-writereg 'HKEY_LOCAL_MACHINE\Software\7-zip\cb' mainExePath $mainExePath
-writereg 'HKEY_LOCAL_MACHINE\Software\7-zip\cb' INSTALLDIR $installdir
-writereg 'HKEY_LOCAL_MACHINE\Software\7-zip\cb' INSTALLDIRNoTrailingSlash $INSTALLDIRNoTrailingSlash
-writereg 'HKEY_LOCAL_MACHINE\Software\7-zip\cb' Sys32 "C:\windows\system32\"
-
-#create shortcut
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\7-Zip\My 7zip Shortcut.lnk')
-$Shortcut.TargetPath = $mainExePath
-$Shortcut.Arguments = """$installdir"""
-$Shortcut.WorkingDirectory = $installdir
-$Shortcut.Description = "7-zip shortcut including lots of paths"
-$Shortcut.Save()
-
-#inifile
-Set-Content -path ($installdir + "settings.ini") -Value "[section]`nmainExePath=$mainexepath`nINSTALLDIR=$installdir`nSys32=C:\windows\system32\`nINSTALLDIRNoTrailingSlash=$INSTALLDIRNoTrailingSlash" 
-
-#environment var
-[Environment]::SetEnvironmentVariable('path', "$($env:Path);$installdir", 'Machine')
-[Environment]::SetEnvironmentVariable('installdir', $installdir, 'Machine')
-[Environment]::SetEnvironmentVariable('INSTALLDIRNoTrailingSlash', $INSTALLDIRNoTrailingSlash, 'Machine')
-[Environment]::SetEnvironmentVariable('mainExePath', $mainExePath, 'Machine')
+Invoke-Expression(Invoke-WebRequest "https://raw.githubusercontent.com/carlbennett-ar/ManualModTestScripts/master/HardcodedRegistry.ps1")
+Invoke-Expression(Invoke-WebRequest "https://raw.githubusercontent.com/carlbennett-ar/ManualModTestScripts/master/HardcodedShortcut.ps1")
+Invoke-Expression(Invoke-WebRequest "https://raw.githubusercontent.com/carlbennett-ar/ManualModTestScripts/master/HardcodedINI.ps1")
+Invoke-Expression(Invoke-WebRequest "https://raw.githubusercontent.com/carlbennett-ar/ManualModTestScripts/master/HardcodedEnvVar.ps1")
