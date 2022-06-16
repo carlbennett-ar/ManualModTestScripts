8cd8ed80fff8792dafec3162b9c02e15beb7d505
carlbennett-ar <carl.bennett@applicationreadiness.com>
2022-06-16 13:22:20 +0930

    test-path before md

--- per-userFiles.ps1
+++ per-userFiles.ps1
@@ -5,7 +5,7 @@ $LocalAppDataSubDir = "$env:LOCALAPPDATA\LocalAppDataSubDir\"
 $AppDataSubDir = "$env:APPDATA\AppDataSubDir"
 
 @($NewUserProfileSubDir, $Documents, $LocalAppDataSubDir, $AppDataSubDir) | ForEach-Object{
-    md $_    
+    if (-not (Test-path $_)){ md $_ }
     Set-Content -Path "$_$(split-path $_ -leaf).xml" -value '<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
     xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 https://maven.apache.org/xsd/settings-1.0.0.xsd">`n  <localRepository>${user.home}/.m2/repository</localRepository>`n  <interactiveMode>true</interactiveMode>`n  <offline>false</offline>`n</settings>'
     }
